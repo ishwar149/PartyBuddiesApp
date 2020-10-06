af839bbc57dcd1b18ea72ae2737e24e9a6e8df05
Ishwar Thind <ishwar3972@gmail.com>
2020-10-06 23:32:10 +1100

    changes

---
+++ PartyBuddiesApp/PartyBuddiesApp/Views/Forms/PartiesTabbed.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PartyBuddiesApp.Views.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PartiesTabbed : TabbedPage
+    {
+        public PartiesTabbed()
+        {
+            InitializeComponent();
+            this.SelectedTabColor = Color.Black;
+            this.UnselectedTabColor = Color.White;
+            this.BarBackgroundColor = Color.FromHex("#EF4873");
+        }
+    }
+}
---
+++ PartyBuddiesApp/PartyBuddiesApp/Views/Forms/PartyDetailsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PartyBuddiesApp.Views.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PartyDetailsPage : ContentPage
+    {
+        public PartyDetailsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
